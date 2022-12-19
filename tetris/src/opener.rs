ae7d5e6f7435f57044e6f2b860d427f405571ffa
Sashimi <hubertliu100@gmail.com>
2022-12-19 11:56:21 -0800

    book

--- tetris/src/opener.rs
+++ tetris/src/opener.rs
@@ -38,96 +38,9 @@ impl Default for OpenerStatus {
 
 impl Default for Opener {
     fn default() -> Self {
-        // NDT
-        // const NDT00: [Piece; 7] = [
-        //     Piece{piece_type: 0, rotation_state: 0, center: Point(1,8), last_kick: 0},
-        //     Piece{piece_type: 1, rotation_state: 0, center: Point(0,4), last_kick: 0},
-        //     Piece{piece_type: 2, rotation_state: 0, center: Point(0,0), last_kick: 0},
-        //     Piece{piece_type: 3, rotation_state: 0, center: Point(1,4), last_kick: 0},
-        //     Piece{piece_type: 4, rotation_state: 1, center: Point(2,6), last_kick: 0},
-        //     Piece{piece_type: 5, rotation_state: 0, center: Point(0,8), last_kick: 0},
-        //     Piece{piece_type: 6, rotation_state: 0, center: Point(3,4), last_kick: 0},
-        // ];
-        //
-        // let DEP00: Dependencies = vec![
-        //     Dependency{ dependency: vec![1, 3, 6] }, // L<S<T
-        //     Dependency{ dependency: vec![5, 0] },    // J<Z
-        // ];
-        //
-        // const NDT01: [Piece; 7] = [
-        //     Piece{piece_type: 0, rotation_state: 3, center: Point(1,4), last_kick: 0},
-        //     Piece{piece_type: 1, rotation_state: 1, center: Point(1,7), last_kick: 0},
-        //     Piece{piece_type: 2, rotation_state: 0, center: Point(0,0), last_kick: 0},
-        //     Piece{piece_type: 3, rotation_state: 1, center: Point(1,8), last_kick: 0},
-        //     Piece{piece_type: 4, rotation_state: 1, center: Point(2,6), last_kick: 0},
-        //     Piece{piece_type: 5, rotation_state: 3, center: Point(1,5), last_kick: 0},
-        //     Piece{piece_type: 6, rotation_state: 0, center: Point(3,4), last_kick: 0},
-        // ];
-        //
-        // let DEP01: Dependencies = vec![
-        //     Dependency{ dependency: vec![5, 6] },    // J<T
-        //     Dependency{ dependency: vec![1, 4] },    // L<I
-        // ];
-        //
-        // const NDT10: [Piece; 7] = [
-        //     Piece{piece_type: 0, rotation_state: 1, center: Point(5,6), last_kick: 0},
-        //     Piece{piece_type: 1, rotation_state: 3, center: Point(5,3), last_kick: 0},
-        //     Piece{piece_type: 2, rotation_state: 0, center: Point(3,7), last_kick: 0},
-        //     Piece{piece_type: 3, rotation_state: 1, center: Point(5,4), last_kick: 0},
-        //     Piece{piece_type: 4, rotation_state: 1, center: Point(4,9), last_kick: 0},
-        //     Piece{piece_type: 5, rotation_state: 1, center: Point(3,0), last_kick: 0},
-        //     Piece{piece_type: 6, rotation_state: 2, center: Point(2,2), last_kick: 0},
-        // ];
-        // let DEP10: Dependencies = vec![ // can be improved, idc
-        //     Dependency{ dependency: vec![2, 0] },   // O<Z
-        //     Dependency{ dependency: vec![0, 6] },   // T must be last
-        //     Dependency{ dependency: vec![1, 6] },
-        //     Dependency{ dependency: vec![2, 6] },
-        //     Dependency{ dependency: vec![3, 6] },
-        //     Dependency{ dependency: vec![4, 6] },
-        //     Dependency{ dependency: vec![5, 6] },
-        // ];
-        //
-        // const NDT11: [Piece; 7] = [
-        //     Piece{piece_type: 0, rotation_state: 1, center: Point(5,6), last_kick: 0},
-        //     Piece{piece_type: 1, rotation_state: 3, center: Point(5,3), last_kick: 0},
-        //     Piece{piece_type: 2, rotation_state: 0, center: Point(3,7), last_kick: 0},
-        //     Piece{piece_type: 3, rotation_state: 1, center: Point(5,4), last_kick: 0},
-        //     Piece{piece_type: 4, rotation_state: 1, center: Point(4,9), last_kick: 0},
-        //     Piece{piece_type: 5, rotation_state: 1, center: Point(3,0), last_kick: 0},
-        //     Piece{piece_type: 6, rotation_state: 2, center: Point(2,2), last_kick: 0},
-        // ];
-        // let DEP11: Dependencies = vec![ // can be improved, idc
-        //                                 Dependency{ dependency: vec![2, 4] },   // O<I
-        //                                 Dependency{ dependency: vec![0, 6] },   // T must be last
-        //                                 Dependency{ dependency: vec![1, 6] },
-        //                                 Dependency{ dependency: vec![2, 6] },
-        //                                 Dependency{ dependency: vec![3, 6] },
-        //                                 Dependency{ dependency: vec![4, 6] },
-        //                                 Dependency{ dependency: vec![5, 6] },
-        // ];
-        //
-        //  Self {
-        //      opener_sequence: vec![vec![NDT00, NDT01], vec![NDT10, NDT11]],
-        //      dependencies_list: vec![vec![DEP00, DEP01], vec![DEP10, DEP11]],
-        //      bag: 0,
-        //      bag_progress: 0,
-        //      variant: 0,
-        //      piece_order: vec![],
-        //      status: Default::default()
-        //  }
-
-
         Self {
-            opener_sequence: vec![vec![[
-                Piece { piece_type: 0, rotation_state: 0, center: Point(1, 4), last_kick: 0 },
-                Piece { piece_type: 1, rotation_state: 1, center: Point(1, 0), last_kick: 0 },
-                Piece { piece_type: 2, rotation_state: 0, center: Point(0, 8), last_kick: 0 },
-                Piece { piece_type: 3, rotation_state: 1, center: Point(1, 6), last_kick: 0 },
-                Piece { piece_type: 4, rotation_state: 0, center: Point(0, 4), last_kick: 0 },
-                Piece { piece_type: 5, rotation_state: 2, center: Point(3, 4), last_kick: 0 },
-                Piece { piece_type: 6, rotation_state: 2, center: Point(1, 2), last_kick: 0 }]]], // always invalid
-            dependencies_list: vec![vec!(vec![Dependency{dependency: vec![4, 0, 5, 6]}, Dependency{dependency: vec![1,6]}, Dependency{dependency: vec![2,6]}])],
+            opener_sequence: vec![],
+            dependencies_list: vec![],
             bag: 0,
             bag_progress: 0,
             variant: 0,
@@ -147,7 +60,9 @@ impl Opener {
     }
 
     pub fn init(&mut self, queue: &PieceOrder) {
-        if self.solve_bag(queue) {
+        if self.opener_sequence.len() == 0 {
+            self.status = OpenerStatus::Invalid;
+        } else if self.solve_bag(queue) {
             self.status = OpenerStatus::Active
         } else {
             self.status = OpenerStatus::Invalid
@@ -209,7 +124,7 @@ impl Opener {
     }
 
     pub fn solve_bag(&mut self, queue: &PieceOrder) -> bool {
-        for i in 1..self.opener_sequence[self.bag].len() {
+        for i in 0..self.opener_sequence[self.bag].len() {
             let mut queue = queue.clone();
             let hold = queue.remove(0);
 
