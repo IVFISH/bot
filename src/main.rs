48940fd69ecb4975d8bf88ecc4a410fc610657dd
Yasmercy <ianchensoftware@gmail.com>
2024-08-15 11:40:25 +0800

    cleanup main code

--- src/main.rs
+++ src/main.rs
@@ -15,69 +15,35 @@ use nontrivials::*;
 use scalar::*;
 use std::time;
 
-// fn main() {
-//     // note: the queue is backwards (also 1-indexed)
-//     let game = Game::new(0x1_321);
-//
-//     let now1 = time::Instant::now();
-//     let res1 = search_scalar(game);
-//     println!(
-//         "scalar found {} boards in {} microseconds",
-//         res1.len(),
-//         now1.elapsed().as_micros()
-//     );
-//
-//     let now2 = time::Instant::now();
-//     let res2 = search_simd(game);
-//     println!(
-//         "simd found {} boards in {} microseconds",
-//         res2[0].len(),
-//         now2.elapsed().as_micros()
-//     );
-// }
+fn bench(game: Game) {
+    // for g in movegen(game) {
+    //     println!(
+    //         "{}",
+    //         Bitmatrix {
+    //             data: g.board.into_iter().collect()
+    //         }
+    //     );
+    // }
 
-fn main() {
-    let mut game = Game::new(0x1);
-    game.board[0] = 0xaaaa;
-    game.board[1] = 0x0000;
-    game.board[2] = 0x2222;
-
-    // game.board[7] = 0x8000;
-    // game.board[8] = 0x36db << 1;
-    // game.board[9] = 0x1249 << 1;
-
-    println!("{}", Bitmatrix { data: game.board.into_iter().collect()});
-
-    // println!("===================");
-
-    let (collisions, trivials) = collision(game);
-    // println!("{}", collisions);
-
-    // println!("===================");
-    
-    let reached = reachable(collisions, trivials);
-    println!("{}", reached[0]);
-
-    for g in movegen(game) {
-        println!(
-            "{}",
-            Bitmatrix {
-                data: g.board.into_iter().collect()
-            }
-        );
-    }
-
-    let rep = 10_000;
+    let rep = 100_000;
     let now = time::Instant::now();
     for _ in 0..rep {
         let _ = movegen(game);
     }
-    println!("movegen took {} microsconds", now.elapsed().as_micros() / rep);
+    println!(
+        "movegen took {} nanoseconds",
+        now.elapsed().as_nanos() / rep
+    );
+}
 
-    let now = time::Instant::now();
-    for _ in 0..rep {
-        let _ = collision(game);
-    }
-    println!("collision map took {} microsconds", now.elapsed().as_micros() / rep);
+fn main() {
+    let mut game = Game::new(0x1);
 
+    bench(game);
+
+    game.board[0] = 0xaaaa;
+    game.board[1] = 0x0000;
+    game.board[2] = 0x2222;
+
+    bench(game);
 }
